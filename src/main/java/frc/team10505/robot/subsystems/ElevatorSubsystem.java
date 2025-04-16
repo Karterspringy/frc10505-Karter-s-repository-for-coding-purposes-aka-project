8d94e379644fa251065f0d599035e6b193086cb2
Karterspringy <pringles4life42@gmail.com>
2025-04-16 19:30:20 -0400

    Added subsystems

---
+++ src/main/java/frc/team10505/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,10 @@
+package frc.team10505.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    //Variables//
+    private final TalonFX elevatorFxLeader = new TalonFX(0);
+    private final TalonFx elevatorFxFollower = new TalonFx(1);
+    private double elevatorEncoderValue = 0.0;
+}
